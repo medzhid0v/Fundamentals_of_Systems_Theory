ae3a094aa7e672e69481129ceb1e3008fe5866fd
Medzhidov Magomedpasha Sultanivich <84726081+jeveungateu@users.noreply.github.com>
2022-12-07 22:01:35 +0300

    Add files via upload

---
+++ rt_7_teor_sistem/practice/api/request.go
@@ -0,0 +1,15 @@
+package api
+
+type Film struct {
+	Title     string `json:"Title"`
+	Year      string `json:"Year"`
+	Released  string `json:"Released"`
+	Runtime   string `json:"Runtime"`
+	Genre     string `json:"Genre"`
+	Director  string `json:"Director"`
+	Writer    string `json:"Writer"`
+	Actors    string `json:"Actors"`
+	Language  string `json:"Language"`
+	Country   string `json:"Country"`
+	Awards    string `json:"Awards"`
+}
